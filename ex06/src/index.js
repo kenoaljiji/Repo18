45a0e766cac1202d56a9453234ac1a52376c8851
kenoaljiji <studioaljiji@gmail.com>
2021-10-01 09:23:36 +0200

    map iterate

---
+++ ex06/src/index.js
@@ -0,0 +1,10 @@
+let rockStar = new Map([
+  ["artist", "Rolling Stones"],
+  ["song", "Angie"],
+  ["album", "Goats Head Soup"],
+  ["singer", "Mick Jagger"],
+]);
+
+let newRock = new Map([...rockStar].filter(([key, value]) => key[0] > "a"));
+console.log(newRock);
+module.exports = { rockStar, newRock };
