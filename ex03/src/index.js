041b2562d883dd3cb4c6b4eaa462a5432b1efbc7
kenoaljiji <studioaljiji@gmail.com>
2021-10-01 07:57:27 +0200

    Iterate a array with generator function

---
+++ ex03/src/index.js
@@ -0,0 +1,15 @@
+function* myGenerator() {
+  yield* [1, 2, 3, 4, 5];
+  yield* "Arena";
+  yield* arguments;
+}
+
+let iterator = myGenerator(6, 7, 8);
+let generatorArray = [];
+
+for (let i of iterator) {
+  generatorArray.push(i);
+}
+
+console.log(generatorArray);
+module.exports = { generatorArray, myGenerator };
